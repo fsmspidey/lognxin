49ca98f62097c4ea679c4a9ec03a869b00ab42be
Fabricio <fsmspidey@gmail.com>
2018-06-10 20:49:38 -0300

    last changes

--- lognxin/views.py
+++ lognxin/views.py
@@ -1,14 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-<<<<<<< Updated upstream
-
-def current_datetime(request):
-	return render(request, 'home/index.html')
-=======
-from data.models import Load
 from django.contrib.sites.models import Site
 
 def index(request):
 	sites = Site.objects.all()
-	return render(request, 'home/index.html', {'sites': sites})
->>>>>>> Stashed changes
+	return render(request, 'home/index.html', {'site_list': sites})
